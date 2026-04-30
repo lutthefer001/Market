477d46dbc7bebb11d138cc742a2f381fe2142587
lutthefer001 <saidtoshmurodov@gmail.com>
2026-04-30 19:43:34 +0500

    Install and configure Unfold Django Admin

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from .models import Product
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(ModelAdmin):
     list_display = ('name', 'price', 'created_at')
     search_fields = ('name', 'description')
     list_filter = ('created_at',)

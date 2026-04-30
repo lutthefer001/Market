fe8f065c72ed3ea25f1d2592e24a4862040e1127
lutthefer001 <saidtoshmurodov@gmail.com>
2026-04-30 20:00:08 +0500

    Update admin URL path to v1/site/admin/admin

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('v1/site/v2/admin/admin/', admin.site.urls),
+    path('v1/site/admin/admin/', admin.site.urls),
     path('', include('store.urls')),
 ]
 
